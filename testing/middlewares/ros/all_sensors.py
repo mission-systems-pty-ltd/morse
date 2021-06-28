4fd2e866fd1fa763a10da20337e4c09aea579a05
Aspen Eyers <aspen.jje@gmail.com>
2021-06-28 13:55:06 +1000

    Added some testing for all of the ros derivers for sensors.
    Still a tad manual...

--- testing/middlewares/ros/all_sensors.py
+++ testing/middlewares/ros/all_sensors.py
@@ -31,69 +31,52 @@ class DataStreamTest(MorseTestCase):
         robot = ATRV()
 
 
-        sensors = [ Accelerometer(),
-        Airspeed(),
-        Armature_pose(),
-        Attitude(),
-        Barometer(),
-        Battery(),
-        Camera(),
-        Clock(),
-        Collision(),
-        Depthaggregator(),
-        Depth_camera_aggregator(),
-        Depth_camera(),
-        Dvl(),
-        EGM9615(),
-        Fls(),
-        GeomagnetismHeader(),
-        GeomagnetismLibrary(),
-        GPS(),
-        Gyroscope(),
-        Human_posture(),
-        Imu(),
-        Laserscanner(),
-        LidarMoos(),
-        Lidar(),
-        Magnetometer(),
-        Magnetometer(),
-        MultiStaticSonarCtrl(),
-        MultiStaticSonar(),
-        MultStaticReciever(),
-        Object(),
-        ObjectServerMoos(),
-        ObjectServer(),
-        Odometry(),
-        OptixCameraMoos(),
-        OptixCamera(),
-        Pose(),
-        Proximity(),
-        Ptu_posture(),
-        Radar_altimeter(),
-        Search_and_rescue(),
-        Semantic_camera(),
-        Sonar(),
-        Stereo_unit(),
-        Thermometer(),
-        Velocity(),
-        Video_camera(),
-        WMM(),
-        Zbufferto3d(),
-        Zbuffertodepth() ]
+        # sensors = [ GPS, Odometry ]
+        
+        sensors = [ Accelerometer,
+            # Airspeed,              # NO ROS DRIVER
+            ArmaturePose,
+            # Attitude,              # NO ROS DRIVER
+            # Barometer,             # NO ROS DRIVER
+            Battery, 
+            # Camera,                # BROKEN
+            Clock,                 # BROKEN
+            # Collision,             # NO ROS DRIVER
+            # DepthCameraAggregator, # BROKEN
+            # DepthCamera,           # BROKEN
+            # DVL,                   # NO ROS DRIVER
+            # FLS,                   # BROKEN
+            GPS, 
+            # Gyroscope,             # BROKEN
+            IMU,            # BROKEN
+            # Lidar,                 # NO ROS DRIVER
+            # Magnetometer,          # BROKEN
+            # MultiStaticSonar,
+            Odometry,
+            # # OptixCamera,
+            # Pose,                  # BROKEN
+            # Proximity,             # BROKEN
+            # PTUPosture,            # BROKEN
+            # RadarAltimeter,        # NO ROS DRIVER
+            # SearchAndRescue,       # NO ROS DRIVER
+            # SemanticCamera,        # BROKEN
+            # Sonar,                 # NO ROS DRIVER
+            # StereoUnit,            # NO ROS DRIVER
+            # Thermometer,           # NO ROS DRIVER
+            Velocity] 
+            # VideoCamera]           # BROKEN
 
-        gps = GPS()
-        gps.add_stream('ros')
-        robot.append(gps)
-
-        odometry = Odometry()
-        odometry.add_stream('ros')
-        robot.append(odometry)
-
-
-
+        for sensor_type in sensors: 
+            print(sensor_type)
+            sensor = sensor_type()
+            topic = sensor.__class__.__name__
+            sensor.add_stream('ros', topic=topic)
+            robot.append(sensor)
 
         env = Environment('empty', fastmode = True)
+        env.properties(latitude = -35.0637135, longitude = 150.735166, altitude = 0)
         env.add_service('socket')
+
 
     def pose_callback(self, data):
         self.pos = data
