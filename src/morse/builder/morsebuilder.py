38bd3e4169e451a2233c7a7336275c367b6c2b6d
Janzen <janzenchoi@yahoo.com>
2023-04-03 10:22:26 +1000

    Fix to allow multiple robots to run at the same time, and fixed odometry and magnetometer tests

--- src/morse/builder/morsebuilder.py
+++ src/morse/builder/morsebuilder.py
@@ -293,6 +293,12 @@ class WheeledRobot(GroundRobot):
                 'WheelRRName', 'CasterWheelName']
         properties = bpymorse.get_properties(self._bpy_object)
         for key in keys:
+            wheel_name = properties.get(key, None)
+            if wheel_name:
+                new_wheel_name = [child.name for child in self._bpy_object.children if wheel_name in child.name][0]
+                bpymorse._property_set(self._bpy_object, key, new_wheel_name)
+        properties = bpymorse.get_properties(self._bpy_object)
+        for key in keys:
             expected_wheel = properties.get(key, None)
             if expected_wheel:
                 wheel = self.get_child(expected_wheel)
