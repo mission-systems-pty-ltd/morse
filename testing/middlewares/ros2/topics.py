9684c3e38514d4cf4a63c6952bedc771c423e768
Henil Patel <henilpatel@missionsystems.com.au>
2024-06-13 11:40:28 +1000

    ros2/topics.py: verify ROS2 support in Morse using rclpy

--- testing/middlewares/ros2/topics.py
+++ testing/middlewares/ros2/topics.py
@@ -6,6 +6,7 @@ This script tests some of the base functionalities of MORSE.
 import math
 # from morse.testing.ros import RosTestCase
 from morse.testing.testing import MorseTestCase, testlogger
+import time 
 
 import rclpy
 from rclpy.impl.implementation_singleton import rclpy_implementation as _rclpy
@@ -15,7 +16,6 @@ from rclpy.utilities import timeout_sec_to_nsec
 import nav_msgs.msg # do not conflict with morse builder
 from geometry_msgs.msg import Twist
 from time import sleep
-from rclpy.wait_for_message import wait_for_message
 
 # Include this import to be able to use your test file as a regular 
 # builder script, ie, usable with: 'morse [run|exec] base_testing.py
@@ -24,65 +24,46 @@ try:
 except ImportError:
     pass
 
+# helper function to publish velocity commands to the robot
 def send_speed(s, v, w, t):
     msg = Twist()
-    msg.linear.x = v
-    msg.angular.z = w
+    msg.linear.x = v # v is the linear velocity
+    msg.angular.z = w # w is the angular velocity
     s.publish(msg)
     sleep(t)
     msg.linear.x = 0.0
     msg.angular.z = 0.0
     s.publish(msg)
 
-# class Subscriber(Node):
+# Subscriber() class to subscribe to the /robot/odometry topic to receive odometry messages
+class Subscriber(Node): 
     
-#     def __init__(self):
-#         super().__init__('morse_ros_data_stream_test')
-#         self.subscription = self.create_subscription(nav_msgs.msg.Odometry, '/robot/odometry', self.pose_callback, 10)
-#         self.subscription # prevent unused variable warning
+    def __init__(self):
+        super().__init__('morse_ros_data_stream_test')
+        self.subscription = self.create_subscription(nav_msgs.msg.Odometry, '/robot/odometry', self.pose_callback, 10)
+        self.subscription # prevent unused variable warning
+        self.pos = None
 
-#     def pose_callback(self, data):
-#         self.pos = data
+    def pose_callback(self, data):
+        self.pos = data
 
-def wait_for_message():
-    """
-    Wait for the next incoming message.
-    :param msg_type: message type
-    :param node: node to initialize the subscription on
-    :param topic: topic name to wait for message
-    :time_to_wait: seconds to wait before returning
-    :return (True, msg) if a message was successfully received, (False, ()) if message
-        could not be obtained or shutdown was triggered asynchronously on the context.
-    """
-    context = node.context
-    wait_set = _rclpy.WaitSet(1, 1, 0, 0, 0, 0, context.handle)
-    wait_set.clear_entities()
+# Publisher() class to publish velocity commands to the /robot/motion topic
+class Publisher(Node):
+    
+    def __init__(self):
+        super().__init__('morse_ros_data_stream_test_publisher')
+        self.publisher = self.create_publisher(Twist, '/robot/motion', 10)
 
-    sub = node.create_subscription(msg_type, topic, lambda _: None, 1)
-    wait_set.add_subscription(sub.handle)
-    sigint_gc = SignalHandlerGuardCondition(context=context)
-    wait_set.add_guard_condition(sigint_gc.handle)
-
-    timeout_nsec = timeout_sec_to_nsec(time_to_wait)
-    wait_set.wait(timeout_nsec)
-
-    subs_ready = wait_set.get_ready_entities('subscription')
-    guards_ready = wait_set.get_ready_entities('guard_condition')
-
-    if guards_ready:
-        if sigint_gc.handle.pointer in guards_ready:
-            return (False, None)
-
-    if subs_ready:
-        if sub.handle.pointer in subs_ready:
-            msg_info = sub.handle.take_message(sub.msg_type, sub.raw)
-            return (True, msg_info[0])
-
-    return (False, None)
-
+    def publish(self, v, w):
+        msg = Twist()
+        msg.linear.x = v
+        msg.linear.z = w
+        self.publisher.publish(msg)
 
 class DataStreamTest(MorseTestCase):
 
+    # Define the test environment and the test case
+    # --- Set up the robot and its sensors and actuators
     def setUpEnv(self):
         """ Defines the test scenario, using the Builder API.
         """
@@ -100,86 +81,74 @@ class DataStreamTest(MorseTestCase):
         env = Environment('empty', fastmode = True)
         env.add_service('socket')
 
-    def pose_callback(self, data):
-        self.pos = data
-
     def test_vw_controller(self):
-        # XXX
-        # test the orientation part, but the easy way is to use numpy or
-        # tf, and don't want to add too much dependency for test
+        # Initialise the ROS2 interface with 'rclpy'
+        rclpy.init()
         
-        # rclpy.init() # initialize the nodecallback_group
-        # node = rclpy.create_node('morse_ros_data_stream_test') # create a node
-        # subscription = node.create_subscription(nav_msgs.msg.Odometry, '/robot/odometry', self.pose_callback, 10)
+        sub = Subscriber()
+        pub = Publisher()
 
-        # rclpy.create_subscription(nav_msgs.msg.Odometry, '/robot/odometry', self.pose_callback)
-        # rclpy.spin_until_future_complete(node, subscription, timeout=10)
+        # wait for the 1st message to be received
+        while sub.pos is None:
+            rclpy.spin_once(sub)
+        
+        precision = 0.15
+        # perform same unit tests as 'ros1.py':
+        self.assertAlmostEqual(sub.pos.pose.pose.position.x, 0.0, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.y, 0.0, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.z, 0.0, delta=precision)
 
-
-        rclpy.init() # can have args=sys.argv as the parameter of init()
-        node = rclpy.create_node('morse_ros_data_stream_test')
-        subscriber = node.create_subscription(nav_msgs.msg.Odometry, '/robot/odometry', self.pose_callback)
-
-        msg = wait_for_message('/robot/odometry', nav_msgs.msg.Odometry, timeout = 10)
-        self.assertIsNotNone(msg)
-
-        cmd_stream = node.create_publisher('/robot/motion', Twist)
-        '''
-        rospy.init_node('morse_ros_data_stream_test')
-        rospy.Subscriber('/robot/odometry', nav_msgs.msg.Odometry, self.pose_callback)
-
-        msg = rospy.client.wait_for_message('/robot/odometry', nav_msgs.msg.Odometry, timeout = 10)
-        self.assertIsNotNone(msg)
-        '''
-
-        # msg = rclpy.client.wait_for_message('/robot/odometry', nav_msgs.msg.Odometry, timeout = 10)
-        # self.assertIsNotNone(msg)
-
-        # cmd_stream = rclpy.Publisher('/robot/motion', Twist)
-       
-        self.assertTrue(hasattr(self, "pos"))
-        precision=0.15
-
-        self.assertAlmostEqual(self.pos.pose.pose.position.x, 0.0, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.y, 0.0, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.z, 0.0, delta=precision)
- 
-        # sleep to make sure that the other peer can read it ...
+        # Wait for motion to complete
         sleep(1)
 
-        send_speed(cmd_stream, 1.0, 0.0, 2.0)
+        # Drive forward
+        send_speed(pub.publisher, 1.0, 0.0, 2.0)
+
+        # Wait for motion to complete - sleep to make sure that the other peer can read it ...
         sleep(1)
 
-        self.assertAlmostEqual(self.pos.pose.pose.position.x, 2.0, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.y, 0.0, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.z, 0.0, delta=precision)
+        # Check new position
+        rclpy.spin_once(sub)
 
-        send_speed(cmd_stream, -1.0, 0.0, 2.0)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.x, 2.0, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.y, 0.0, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.z, 0.0, delta=precision)
 
-        self.assertAlmostEqual(self.pos.pose.pose.position.x, 0.0, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.y, 0.0, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.z, 0.0, delta=precision)
-
-        # sleep to make sure that the other peer can read it ...
+        # Wait for motion to complete
         sleep(1)
 
-        send_speed(cmd_stream, 1.0, -math.pi/4.0, 2.0)
+        # Drive backward
+        send_speed(pub.publisher, -1.0, 0.0, 2.0) # go back to where it was initially landed
+
+        # Wait for motion to complete
         sleep(1)
 
-        self.assertAlmostEqual(self.pos.pose.pose.position.x, 4.0 / math.pi, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.y, -4.0 / math.pi, delta=precision)
-        self.assertAlmostEqual(self.pos.pose.pose.position.z, 0.0, delta=precision)
+        # Check new position
+        rclpy.spin_once(sub)
 
-        # node.destroy_node()
-        # rclpy.shutdown()
+        # all x,y,z values should be 0.0 as it was initially
+        self.assertAlmostEqual(sub.pos.pose.pose.position.x, 0.0, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.y, 0.0, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.z, 0.0, delta=precision)
 
+        # Wait for motion to complete
+        sleep(1)
 
+        # Drive forward and rotate -math.pi/4.0
+        send_speed(pub.publisher, 1.0, -math.pi/4.0, 2.0)
 
-# def main(args=None):
-#     rclpy.init(args=args)
-#     subscriber = DataStreamTest()
+        # Wait for motion to complete
+        sleep(1)
 
-#     rclpy.spin(subscriber)
+        # Check new position
+        rclpy.spin_once(sub)
+
+        self.assertAlmostEqual(sub.pos.pose.pose.position.x, 4.0 / math.pi, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.y, -4.0 / math.pi, delta=precision)
+        self.assertAlmostEqual(sub.pos.pose.pose.position.z, 0.0, delta=precision)
+
+        # Close ROS2 interface
+        rclpy.shutdown()
 
 ########################## Run these tests ##########################
 if __name__ == "__main__":
